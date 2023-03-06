bd9d7f3449c8893e14a7c1045bd439c885b60b50
Dinos Papakostas <dinospk1999@gmail.com>
2023-03-06 17:14:54 +0100

    Add support for more template types

--- inpars/prompt.py
+++ inpars/prompt.py
@@ -1,23 +1,31 @@
+import json
 import os
-import ftfy
-import yaml
 import random
 
-with open(f'{os.path.dirname(__file__)}/prompts/templates.yaml') as f:
+import ftfy
+import yaml
+from pyserini.search.lucene import LuceneSearcher
+
+with open(f"{os.path.dirname(__file__)}/prompts/templates.yaml") as f:
     templates = yaml.safe_load(f)
 
 
 class Prompt:
     def __init__(
-            self,
-            template=None,
-            examples=None,
-            tokenizer=None,
-            max_doc_length=None,
-            max_query_length=None,
-            max_prompt_length=None,
-            max_new_token=16,
+        self,
+        index=None,
+        instruction=None,
+        template=None,
+        examples=None,
+        tokenizer=None,
+        max_doc_length=None,
+        max_query_length=None,
+        max_prompt_length=None,
+        max_new_token=16,
+        **kwargs,
     ):
+        self.index = index
+        self.instruction = instruction
         self.template = template
         self.examples = examples
         self.tokenizer = tokenizer
@@ -31,14 +39,17 @@ class Prompt:
         if name in templates:
             template = templates[name]
             prompt_class = {
-                'dynamic': DynamicPrompt,
-                'static': StaticPrompt,
-                'chat': ChatPrompt,
-            }[template['mode']]
-            return prompt_class(template=template['template'], *args, **kwargs)
+                "dynamic": DynamicPrompt,
+                "static": StaticPrompt,
+                "instruction": InstructionPrompt,
+                "co-instruction": ContrastiveInstructionPrompt,
+                "chat": ChatPrompt,
+                "co-chat": ContrastiveChatPrompt,
+            }[template["mode"]]
+            return prompt_class(*args, **template, **kwargs)
         else:
             if not os.path.exists(name):
-                raise FileNotFoundError(f'Prompt file {name} was not found!')
+                raise FileNotFoundError(f"Prompt file {name} was not found!")
 
             with open(name) as f:
                 return StaticPrompt(template=f.read(), *args, **kwargs)
@@ -46,9 +57,11 @@ class Prompt:
     def _truncate_max_doc_length(self, document):
         if self.max_doc_length:
             document = self.tokenizer.decode(
-                self.tokenizer(document, truncation=True, max_length=self.max_doc_length)[
-                    "input_ids"
-                ]
+                self.tokenizer(
+                    document,
+                    truncation=True,
+                    max_length=self.max_doc_length,
+                )["input_ids"]
             )
         return document
 
@@ -74,23 +87,25 @@ class DynamicPrompt(Prompt):
     def build(self, document, n_examples=3):
         random_examples = random.sample(self.examples, n_examples)
 
-        prompt = ""
+        prompt = self._get_base_prompt()
         for i in range(n_examples):
             _, _, query, doc = random_examples[i]
             query = self._truncate_max_query_length(query)
             doc = self._truncate_max_doc_length(doc)
 
-            prompt += self.template.format(document=doc, query=query)
+            prompt += self._format_template(doc, query, i)
 
         document = ftfy.fix_text(document)
         if self.max_doc_length:
             document = self.tokenizer.decode(
-                self.tokenizer(document, truncation=True, max_length=self.max_doc_length)[
-                    "input_ids"
-                ]
+                self.tokenizer(
+                    document,
+                    truncation=True,
+                    max_length=self.max_doc_length,
+                )["input_ids"]
             )
 
-        prompt += self.template.format(document=document, query="").rstrip()
+        prompt += self._format_template(document, "", n_examples).rstrip()
 
         if self.max_prompt_length:
             prompt_length = len(self.tokenizer.tokenize(prompt))
@@ -102,22 +117,78 @@ class DynamicPrompt(Prompt):
 
         return prompt
 
+    def _get_base_prompt(self):
+        return ""
+
+    def _format_template(self, document, query, example_idx):
+        args = {
+            "document": document,
+            "query": query,
+        }
+        if "example_idx" in self.template:
+            args["example_idx"] = example_idx + 1
+
+        return self.template.format(**args)
+
     def _truncate_max_query_length(self, query):
         if self.max_query_length:
-                query = self.tokenizer.decode(
-                    self.tokenizer(query, truncation=True, max_length=self.max_query_length)[
-                        "input_ids"
-                    ]
-                )
+            query = self.tokenizer.decode(
+                self.tokenizer(
+                    query,
+                    truncation=True,
+                    max_length=self.max_query_length,
+                )["input_ids"]
+            )
         return query
+
+
+class InstructionPrompt(DynamicPrompt):
+    def _get_base_prompt(self):
+        return self.instruction + " Only respond with the generated query.\n\n"
+
+
+class ContrastiveInstructionPrompt(InstructionPrompt):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.topk = 50
+
+        if os.path.isdir(self.index):
+            self.searcher = LuceneSearcher(self.index)
+        else:
+            self.searcher = LuceneSearcher.from_prebuilt_index(self.index)
+
+    def _get_base_prompt(self):
+        return (
+            self.instruction
+            + " However, make sure that the generated query does not follow the "
+            + 'instruction for the "Irrelevant Document". Only respond with the '
+            + "generated query.\n\n"
+        )
+
+    def _format_template(self, rel_document, query, example_idx):
+        results = self.searcher.search(rel_document, k=self.topk + 1)
+        hit = json.loads(results[self.topk].raw)
+        irr_document = ftfy.fix_text(
+            f"{hit['title']} {hit['text']}" if hit["title"] else hit["text"]
+        )
+
+        args = {
+            "rel_document": rel_document,
+            "irr_document": irr_document,
+            "query": query,
+        }
+        if example_idx in self.template:
+            args["example_idx"] = example_idx + 1
+
+        return self.template.format(**args)
+
 
 class ChatPrompt(DynamicPrompt):
     def build(self, document, n_examples=3):
         random_examples = random.sample(self.examples, n_examples)
 
-        messages = [
-            {"role": "system", "content": self.template}
-        ]
+        messages = [{"role": "system", "content": self._get_instruction()}]
 
         for i in range(n_examples):
             _, _, query, doc = random_examples[i]
@@ -125,20 +196,20 @@ class ChatPrompt(DynamicPrompt):
             doc = self._truncate_max_doc_length(doc)
 
             messages += [
-                {"role": "user", "content": query},
-                {"role": "assistant", "content": doc},
+                {"role": "user", "content": self._format_template(doc)},
+                {"role": "assistant", "content": query},
             ]
 
         document = ftfy.fix_text(document).rstrip()
         if self.max_doc_length:
             document = self.tokenizer.decode(
-                self.tokenizer(document, truncation=True, max_length=self.max_doc_length)[
-                    "input_ids"
-                ]
+                self.tokenizer(
+                    document, truncation=True, max_length=self.max_doc_length
+                )["input_ids"]
             )
 
         messages += [
-            {"role": "user", "content": document},
+            {"role": "user", "content": self._format_template(document)},
         ]
 
         if self.max_prompt_length:
@@ -151,3 +222,40 @@ class ChatPrompt(DynamicPrompt):
                 )
 
         return messages
+
+    def _get_instruction(self):
+        return self.instruction + " Only respond with the generated query."
+
+    def _format_template(self, document):
+        return self.template.format(document=document).rstrip()
+
+
+class ContrastiveChatPrompt(ChatPrompt):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.topk = 50
+
+        if os.path.isdir(self.index):
+            self.searcher = LuceneSearcher(self.index)
+        else:
+            self.searcher = LuceneSearcher.from_prebuilt_index(self.index)
+
+    def _get_instruction(self):
+        return (
+            self.instruction
+            + " However, make sure that the generated query does not follow the "
+            + 'instruction for the "Irrelevant Document". Only respond with the '
+            + "generated query."
+        )
+
+    def _format_template(self, rel_document):
+        results = self.searcher.search(rel_document, k=self.topk + 1)
+        hit = json.loads(results[self.topk].raw)
+        irr_document = ftfy.fix_text(
+            f"{hit['title']} {hit['text']}" if hit["title"] else hit["text"]
+        )
+        return self.template.format(
+            rel_document=rel_document,
+            irr_document=irr_document,
+        ).rstrip()
