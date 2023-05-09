ccad09a151ed37a937ba4249a02d985cb311d5c5
Dinos Papakostas <dinospk1999@gmail.com>
2023-05-09 21:34:01 +0200

    Smaller batch size for smaller CE training

--- _custom/train_ce.sh
+++ _custom/train_ce.sh
@@ -19,10 +19,10 @@ python -m inpars.train_minilm \
     --triples $OUTPUT_DIR/triples.tsv \
     --base_model cross-encoder/ms-marco-MiniLM-L-6-v2 \
     --output_dir $OUTPUT_DIR/models/miniLM \
-    --per_device_train_batch_size 128 \
+    --per_device_train_batch_size 32 \
     --learning_rate 3e-6 \
     --weight_decay 5e-5 \
-    --warmup_steps 156 \
-    --max_steps 156 \
-    --optim adamw_bnb_8bit \
+    --warmup_ratio 0.1 \
+    --max_steps 624 \
+    --optim adamw_torch \
     --bf16
